6e7e8a46dc1e5f0b77280fc7059bac55243e7edb
Ahmed <barud546@gmail.com>
2025-08-22 08:23:31 +0100

    Add EB env vars & RDS; Terraform config

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_elastic_beanstalk_environment" "ch_aa_beanstalk_app_environment" {
     name      = "EC2KeyName"
     value     = "CHTerraform"
   }
-  
+
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "DB_HOST"
